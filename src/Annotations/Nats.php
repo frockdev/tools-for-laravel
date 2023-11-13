94d7aef57906e326b66b513f9cf84db534e573ee
vladimir zakotnev <vladitot@gmail.com>
2023-11-13 15:31:18 +0300

    fixed nats annotation

--- src/Annotations/Nats.php
+++ src/Annotations/Nats.php
@@ -6,13 +6,17 @@ namespace FrockDev\ToolsForLaravel\Annotations;
 class Nats
 {
     public string $subject;
+    public ?string $queueGroup = null;
 
     public ?string $stream = null;
+    public ?string $consumerName = null;
 
-    public function __construct(string $subject, ?string $stream = null)
+    public function __construct(string $subject, ?string $queueGroup = null, ?string $stream = null, ?string $consumerName = null)
     {
         $this->subject = $subject;
         $this->stream = $stream;
+        $this->consumerName = $consumerName;
+        $this->queueGroup = $queueGroup;
     }
 
 }
