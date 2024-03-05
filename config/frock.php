e0207a77e47470a84f786894412ec3993fdc0ae5
Vladimir <vladimir@sobaka.com>
2024-03-05 22:13:21 +0100

    fix config

--- config/frock.php
+++ config/frock.php
@@ -1,19 +1,8 @@
 <?php
 
 return [
-    'preserveObjects'=>[
-        'config',
-        'app',
-        "path",
-        "path.base",
-        "path.config",
-        "path.database",
-        "path.public",
-        "path.resources",
-        "path.storage",
-        "path.bootstrap",
-        "path.lang",
-        "app",
-        Illuminate\Container\Container::class,
-    ],
+
+    'interStreamInstances'=>[
+
+    ]
 ];
