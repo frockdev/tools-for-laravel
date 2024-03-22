83606df463bb744e4030a2fcc1fb33e8388b0720
Vladimir <vladimir@sobaka.com>
2024-03-22 16:17:02 +0100

    fixing Abstract message

--- src/Transport/AbstractMessage.php
+++ src/Transport/AbstractMessage.php
@@ -3,6 +3,8 @@
 namespace FrockDev\ToolsForLaravel\Transport;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Support\Transformation\TransformationContext;
+use Spatie\LaravelData\Support\Transformation\TransformationContextFactory;
 
 abstract class AbstractMessage extends Data
 {
@@ -11,5 +13,29 @@ abstract class AbstractMessage extends Data
     public function __construct()
     {
         $this->except('context');
+    }
+
+    protected array $excepted = [];
+
+    public function except(string ...$except): static
+    {
+        $this->excepted = array_merge($this->excepted, $except);
+        return parent::except(...$this->excepted);
+    }
+
+    protected array $onlyed = [];
+
+    public function only(string ...$only): static
+    {
+        $this->onlyed = array_merge($this->onlyed, $only);
+        return parent::only(...$this->onlyed);
+    }
+
+    public function transform(TransformationContext|TransformationContextFactory|null $transformationContext = null,): array
+    {
+        $result =  parent::transform($transformationContext);
+        parent::except(...$this->excepted);
+        parent::only(...$this->onlyed);
+        return $result;
     }
 }
