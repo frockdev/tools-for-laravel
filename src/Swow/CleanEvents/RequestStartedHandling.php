359b5c76722370748394d22e4ab048f56c98e232
Vladimir <vladimir@sobaka.com>
2024-04-02 14:03:15 +0200

    added DB disconnection after request handled

--- src/Swow/CleanEvents/RequestStartedHandling.php
+++ src/Swow/CleanEvents/RequestStartedHandling.php
@@ -2,13 +2,18 @@
 
 namespace FrockDev\ToolsForLaravel\Swow\CleanEvents;
 
+use FrockDev\ToolsForLaravel\Swow\ContextStorage;
 use Illuminate\Http\Request;
 
 class RequestStartedHandling
 {
+    private ?\Illuminate\Foundation\Application $app;
+    private ?\Illuminate\Foundation\Application $sandbox;
+
     public function __construct(
         public Request $request
     ) {
-
+        $this->app = ContextStorage::getApplication();
+        $this->sandbox = ContextStorage::getApplication();
     }
 }
