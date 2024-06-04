e2234041adac0b8ef78111483044106ef49a1fdb
vladimir zakotnev <vladitot@gmail.com>
2024-06-04 14:19:02 +0200

    fixed Collector and run order

--- src/AnnotationsCollector/Collector.php
+++ src/AnnotationsCollector/Collector.php
@@ -8,11 +8,26 @@ use Nette\PhpGenerator\ClassType;
 
 class Collector
 {
+    private static mixed $registeredPackagePaths = [];
     private Application $app;
 
-    public function __construct(Application $app)
+    private function __construct(Application $app)
     {
         $this->app = $app;
+    }
+
+    private static ?self $instance = null;
+
+    public static function getInstance(): self {
+        if (self::$instance===null) {
+            self::$instance = new self(app());
+        }
+        return self::$instance;
+    }
+
+    public static function addPackageToCollector(string $packagePath): void
+    {
+        self::$registeredPackagePaths[] = $packagePath;
     }
 
     public function collect(?string $path = null): void
@@ -36,6 +51,7 @@ class Collector
 
         if ($cachedHash!=$currentHash) {
             $classes = $this->getFilesWithClassesRecursively($path);
+            $classes = $this->addClassesFromRegisteredPackages($classes);
             $classBasedThree = $this->createClassBasedThree($classes);
             $attributeBasedThree = $this->createAttributeBasedThree($classBasedThree);
             file_put_contents(
@@ -139,87 +155,6 @@ class Collector
         }
         return $three;
     }
-//    {
-//        $three = [];
-//        foreach ($classes as $filePath) {
-//            $content = file_get_contents($filePath);
-//            //lets get namespace of class with regex
-//            $regex = '/^namespace\s+(.*);/m';
-//            preg_match($regex, $content, $matches);
-//            if (!isset($matches[1])) {
-//                continue;
-//            }
-//            $namespace = $matches[1];
-//            try {
-//                /** @var ClassType $classType */
-//                $classType = ClassType::fromCode($content);
-//            } catch (\Throwable $e) {
-//                continue;
-//            }
-//
-//            $classAttributes = $classType->getAttributes();
-//            $fullClassName = '\\'.$namespace.'\\'.$classType->getName();
-//            if (count($classAttributes)>0) {
-//                foreach ($classAttributes as $attribute) {
-//                    if (!array_key_exists($attribute->getName(), $three)) $three[$attribute->getName()] = [];
-//                    if (!array_key_exists('classAttributes', $three[$attribute->getName()])) $three[$attribute->getName()]['classAttributes'] = [];
-//                    if (!array_key_exists($fullClassName, $three[$attribute->getName()]['classAttributes'])) $three[$attribute->getName()]['classAttributes'][$fullClassName] = [];
-//                    foreach ($attribute->getArguments() as $key=>$argument) {
-//                        $three[$attribute->getName()]['classAttributes'][$fullClassName][$key] = $argument;
-//                    }
-//                }
-//            }
-//
-//            foreach ($classType->getMethods() as $method) {
-//                $methodAttributes = $method->getAttributes();
-//                if (count($methodAttributes)>0) {
-//                    foreach ($methodAttributes as $attribute) {
-//                        if (!array_key_exists($attribute->getName(), $three)) $three[$attribute->getName()] = [];
-//                        if (!array_key_exists('methodAttributes', $three[$attribute->getName()])) $three[$attribute->getName()]['methodAttributes'] = [];
-//                        if (!array_key_exists($fullClassName, $three[$attribute->getName()]['methodAttributes'])) $three[$attribute->getName()]['methodAttributes'][$fullClassName] = [];
-//                        if (!array_key_exists($method->getName(), $three[$attribute->getName()]['methodAttributes'][$fullClassName])) $three[$attribute->getName()]['methodAttributes'][$fullClassName][$method->getName()] = [];
-//                        foreach ($attribute->getArguments() as $key=>$argument) {
-//                            $three[$attribute->getName()]['methodAttributes'][$fullClassName][$method->getName()][$key] = $argument;
-//                        }
-//                    }
-//                }
-//            }
-//
-//            foreach ($classType->getProperties() as $property) {
-//                $propertyAttributes = $property->getAttributes();
-//                if (count($propertyAttributes)>0) {
-//                    foreach ($propertyAttributes as $attribute) {
-//                        if (!array_key_exists($attribute->getName(), $three)) $three[$attribute->getName()] = [];
-//                        if (!array_key_exists('propertyAttributes', $three[$attribute->getName()])) $three[$attribute->getName()]['propertyAttributes'] = [];
-//                        if (!array_key_exists($fullClassName, $three[$attribute->getName()]['propertyAttributes'])) $three[$attribute->getName()]['propertyAttributes'][$fullClassName] = [];
-//                        if (!array_key_exists($property->getName(), $three[$attribute->getName()]['propertyAttributes'][$fullClassName])) $three[$attribute->getName()]['propertyAttributes'][$fullClassName][$property->getName()] = [];
-//                        foreach ($attribute->getArguments() as $key=>$argument) {
-//                            $three[$attribute->getName()]['propertyAttributes'][$fullClassName][$property->getName()][$key] = $argument;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        //lets clean up empty arrays on lowest levels
-//        foreach ($three as $attributeName=>$attribute) {
-//            foreach ($attribute as $attributeType=>$attributeTypeValue) {
-//                foreach ($attributeTypeValue as $className=>$class) {
-//                    if (count($class)===0) {
-//                        unset($three[$attributeName][$attributeType][$className]);
-//                    }
-//                }
-//                if (count($attributeTypeValue)===0) {
-//                    unset($three[$attributeName][$attributeType]);
-//                }
-//            }
-//            if (count($attribute)===0) {
-//                unset($three[$attributeName]);
-//            }
-//        }
-//
-//        return $three;
-//    }
 
     private function createClassBasedThree(mixed $classes)
     {
@@ -339,6 +274,17 @@ class Collector
             }
         }
         return $hash;
+    }
+
+    private function addClassesFromRegisteredPackages(array $classes): array
+    {
+        foreach (self::$registeredPackagePaths as $path) {
+            $classes = $this->getFilesWithClassesRecursively($path);
+            foreach ($classes as $class) {
+                $classes[] = $class;
+            }
+        }
+        return $classes;
     }
 
 }
