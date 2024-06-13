33b361c828a64785fe726f07053f17a3d7546c00
Vladimir <vladimir@sobaka.com>
2024-06-13 12:12:23 +0200

    Content type set

--- src/Swow/Processes/RpcHttpProcess.php
+++ src/Swow/Processes/RpcHttpProcess.php
@@ -69,6 +69,7 @@ class RpcHttpProcess extends AbstractProcess
                                         );
                                         $laravelRequest = Request::createFromBase($symfonyRequest);
                                         $laravelRequest->headers->set('Accept', 'application/json');
+                                        $laravelRequest->headers->set('Content-Type', 'application/json');
                                         $dispatcher = app()->make(\Illuminate\Contracts\Events\Dispatcher::class);
                                         $dispatcher->dispatch(new RequestReceived(ContextStorage::getMainApplication(), app(), $laravelRequest));
                                         /** @var HttpKernelContract $kernel */
